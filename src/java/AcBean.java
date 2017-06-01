d9e1cc0c3cc231b11692e789dca1d5ae59f52fcb
tux <tux@tux-pc>
2017-06-01 13:40:32 -0300

    teste

---
+++ src/java/AcBean.java
@@ -0,0 +1,69 @@
+
+import com.google.gson.Gson;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author tux
+ */
+@Named
+@RequestScoped
+public class AcBean {
+    private List<Json> list;
+    
+    
+    class Json {
+        private String nome;
+        private String idade;
+
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public String getIdade() {
+            return idade;
+        }
+
+        public void setIdade(String idade) {
+            this.idade = idade;
+        }
+
+        public Json(String nome, String idade) {
+            this.nome = nome;
+            this.idade = idade;
+        }
+        
+           
+    }
+    
+    
+    @PostConstruct
+    private void onInit(){
+        Json j1 = new Json("Ai saulinho" , "24");
+        Json j2 = new Json("Cavalo", "25");
+        Json j3  = new Json("Cagurus esvoazantes" , "25");
+        list = new ArrayList<>();
+        list.add(j1);list.add(j2);list.add(j3);
+        
+    }
+    
+    public String getJson(){
+       return new Gson().toJson(list);
+    }
+}
